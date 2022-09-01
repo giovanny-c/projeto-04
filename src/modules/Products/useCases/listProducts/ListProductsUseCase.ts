a9b0eb9f845ab457d61e717c2721f212667c62ff
Giovanny Castanharo <giovanycast@hotmail.com>
2022-08-31 21:31:43 -0300

    add get files for list products

--- src/modules/Products/useCases/listProducts/ListProductsUseCase.ts
+++ src/modules/Products/useCases/listProducts/ListProductsUseCase.ts
@@ -1,4 +1,7 @@
 import { IUsersRepository } from "@modules/Accounts/repositories/IUsersRepository";
+import { File } from "@modules/File/entities/File";
+import { FileMap } from "@modules/File/mapper/FileMap";
+import { IFileRepository } from "@modules/File/repositories/IFileRepository";
 import { IFindProducts } from "@modules/Products/dtos/IFindProductsDTO";
 import { IProductsRepository } from "@modules/Products/repositories/IProductsRepository";
 import { inject, injectable } from "tsyringe";
@@ -13,14 +16,16 @@ class ListProductsUseCase {
         @inject("ProductsRepository")
         private productsRepository: IProductsRepository,
         @inject("UsersRepository")
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        @inject("FileRepository")
+        private fileRepository: IFileRepository
     ) {
 
     }
 
     async execute({ search_query, available, vendor_name, price_range, order_by, limit, offset }: IRequestListProducts) {
 
-        return await this.productsRepository.find({
+        const products = await this.productsRepository.find({
             search_query,
             available,
             vendor_name,
@@ -29,6 +34,26 @@ class ListProductsUseCase {
             limit,
             offset
         })
+
+        let response = products.map(async (product) => {
+
+            let files = await this.fileRepository.getFileUrlByIdOrProductId({ product_id: product.id }) as File[]
+
+            let filesUrl = files.map(file => {
+
+                return FileMap.return_URL(file)
+
+            });
+
+            return {
+                product,
+                filesUrl
+            }
+
+
+        });
+
+        return response
 
     }
 
