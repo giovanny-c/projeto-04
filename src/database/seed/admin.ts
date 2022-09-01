1d01ad6f6e362b0cff4af199a80710740639be2e
Giovanny Castanharo <giovanycast@hotmail.com>
2022-09-01 20:12:47 -0300

    refactoring list products

---
+++ src/database/seed/admin.ts
@@ -0,0 +1,35 @@
+
+
+const dataSource = require("../index")
+const User = require("../../modules/Accounts/entities/User")
+const { genPassword } = require("../../utils/passwordUtils")
+//import { dataSource } from "database"
+//import { User } from "@modules/Accounts/entities/User"
+//import { genPassword } from "@utils/passwordUtils/passwordUtils"
+
+async function createAdmin() {
+
+    const repository = dataSource.getRepository(User)
+
+    const password = "123456"
+    const { hash, salt } = genPassword(password)
+
+    const user = repository.create({
+        name: "admin",
+        email: "admin@email.com",
+        password_hash: hash,
+        salt,
+        is_confirmed: true,
+        admin: true
+    })
+
+    await this.repository.save(user)
+
+    console.log(`Admin created, login: ${user.email}, password: ${password}`)
+
+    dataSource.destroy()
+
+
+
+
+}
