5363f767441ab1b2fcb26a6ad907b0632d7711d3
Giovanny Castanharo <giovanycast@hotmail.com>
2022-10-22 21:07:03 -0300

    changing vendors orders use case to order module

--- src/database/seed/admin.ts
+++ src/database/seed/admin.ts
@@ -1,36 +1,36 @@
 
 
-const dataSource = require("../index")
-const User = require("../../modules/Accounts/entities/User")
-const { genPassword } = require("../../utils/passwordUtils")
-//import { dataSource } from "database"
-//import { User } from "@modules/Accounts/entities/User"
-//import { genPassword } from "@utils/passwordUtils/passwordUtils"
+// import {dataSorce} from "../index" 
+// const User = require("../../modules/Accounts/entities/User")
+// const { genPassword } = require("../../utils/passwordUtils")
+// //import { dataSource } from "database"
+// //import { User } from "@modules/Accounts/entities/User"
+// //import { genPassword } from "@utils/passwordUtils/passwordUtils"
 
-async function createAdmin() {
+// async function createAdmin() {
 
-    const repository = dataSource.getRepository(User)
+//     const repository = dataSource.getRepository(User)
 
-    const password = "123456"
-    const { hash, salt } = genPassword(password)
+//     const password = "123456"
+//     const { hash, salt } = genPassword(password)
 
-    const user = repository.create({
-        name: "admin",
-        email: "admin@email.com",
-        password_hash: hash,
-        salt,
-        is_confirmed: true,
-        admin: true
-    })
+//     const user = repository.create({
+//         name: "admin",
+//         email: "admin@email.com",
+//         password_hash: hash,
+//         salt,
+//         is_confirmed: true,
+//         admin: true
+//     })
 
-    await this.repository.save(user)
+//     await this.repository.save(user)
 
-    console.log(`Admin created, login: ${user.email}, password: ${password}`)
+//     console.log(`Admin created, login: ${user.email}, password: ${password}`)
 
-    dataSource.destroy()
+//     dataSource.destroy()
 
-    //Nao ta FUnCionado
+//     //Nao ta FUnCionado
 
 
 
-}
+// }
