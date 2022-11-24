ec3be6c2a34b9bab59b2add8514a26379ca8e555
Giovanny Castanharo <giovanycast@hotmail.com>
2022-11-23 22:49:49 -0300

    add shipping provider

---
+++ src/shared/container/providers/shippingProvider/implementations/CorreiosShippingProvider.ts
@@ -0,0 +1,57 @@
+import {calcularPrecoPrazo, consultarCep, rastrearEncomendas, CepResponse, PrecoPrazoRequest} from "correios-brasil"
+
+
+class CorreiosShippingProvider {
+
+
+    //consultar cep
+    async validZipcode(zipcode: string): Promise<CepResponse>{
+
+        return await consultarCep(zipcode)
+    
+         
+    }
+    
+    
+
+    //calcular preços e prazos
+    async valculatePriceAndDeliveryTime({
+        vendorFacilityZipcode,
+        customerZipcode,
+        productWeight,
+        productShape,
+        productLenght,
+        productHeight,
+        productWidth,
+        typeOfService,
+        productDiameter,
+
+    }){
+       
+        let args = {
+        sCepOrigem: vendorFacilityZipcode,
+        sCepDestino: customerZipcode,
+        nVlPeso: productWeight,
+        nCdFormato: productShape,
+        nVlComprimento: productLenght,
+        nVlAltura: productHeight,
+        nVlLargura: productWidth,
+        nCdServico: typeOfService ,
+        nVlDiametro: productDiameter,
+        } 
+
+        return await calcularPrecoPrazo(args)
+
+    }
+
+
+    async orderTracking(trackingCode: string){
+
+        return await rastrearEncomendas(trackingCode)
+    }
+
+
+    //rastrear emcomenda
+}
+//ver melhor os docs
+export{CorreiosShippingProvider}
